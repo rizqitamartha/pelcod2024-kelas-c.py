7df6dda97ae2788c996f86630f5418287b360d84
rizqitamartha <rizqitamartha@gmail.com>
2024-09-19 21:42:29 +0700

    Add files via upload

---
+++ 240441100027_RIZQITA MARTHA AMALIA_PELCOD2024-Kelas-C.py
@@ -0,0 +1,36 @@
+# #Mendefinisikan karakter/nama orang
+# nama1 = "Jono"
+# nama2 = "Joni"
+
+# #Percakapan pertama
+# print(f"{nama1} : Hai {nama2}, lama ga ketemu gimana kabarmu? ")
+# print(f"{nama2} : Alhamdulillah baik, keliatannya kamu juga sehat {nama1} ")
+
+# print(f"{nama1} : iyaa kabarku juga baik, eh aku ada tebak-tebakan kamu main ga? (Iya/Enggak) ")
+# Jawaban=input(f"{nama2} : ")
+
+# if Jawaban == "Iya":
+#     print(f"{nama2} : Iya boleh banget ")
+# else:
+#     print(f"{nama2} : Enggak dulu deh ")
+
+# print(f"{nama1} : Ayo kita mulai mainnya! ")
+
+# #Input mendapatkan data
+# bilangan1=int(input(f"{nama1} : Berat Badanku (Masukkan Angka) : "))
+# bilangan2=int(input(f"{nama2} : Sedangkan Berat Badanku (Masukkan Angka) : "))
+
+# #Menghitung Selisih Berat Badan
+# Selisih_BeratBadan =abs(bilangan1 - bilangan2)
+
+# # Operasi logika selisih berat badan
+# if bilangan1>bilangan2:
+#     print(f"{nama2} : Badanku kecil ya dari berat badanmu {Selisih_BeratBadan} ")
+# else:
+#     print(f"{nama2} : Kamu gede juga ternyata {Selisih_BeratBadan} ")
+
+# #Percakapan terakhir
+# print(f"{nama1} : Keliatannya kamu lebih banyak makan lagi biar keliatan lebih gede ")
+# print(f"{nama2} : Haha iyaa habis ini aku mau makan banyak")
+# print(f"{nama1} : Ini udah siang, sampai jumpa lagi ya senang bisa bertemu denganmu")
+# print(f"{nama2} : Iya see you hati-hati dijalan ")
